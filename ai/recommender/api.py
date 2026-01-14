dba56e5ce874654d9d5525fe4c1d805c9d3343ab
Kachigalla Vaishnavi <kachigallavaishnavi@gmail.com>
2026-01-14 15:25:26 +0530

    resolve error

--- ai/recommender/api.py
+++ ai/recommender/api.py
@@ -73,4 +73,6 @@ def recommend():
     return jsonify(recommend_model(drink))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
